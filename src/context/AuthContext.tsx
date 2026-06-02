6addfa74c56022e67f7f95cebb579470e1fba85b
fateh moonesi <f.moonesi@jsk.local>
2026-06-02 17:02:59 +0330

    robots.ts added

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,67 +1,3 @@
-// // context/AuthContext.tsx
-// "use client";
-// import { createContext, useContext, useState, ReactNode, useEffect } from "react";
-// import { authAPI } from "@/lib/api";
-
-// interface User {
-//   id: number;
-//   name: string;
-//   email:string;
-//   mobile: string;
-//   is_admin:boolean;
-//   bank_card_number:string;
-//   bank_card_owner:string;
-// }
-
-// interface AuthContextType {
-//   user: User | null;
-//   setUser: (user: User | null) => void;
-//   loading: boolean;
-//   refreshUser: () => Promise<void>;
-// }
-
-// const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-// export function AuthProvider({ children }: { children: ReactNode }) {
-//   const [user, setUser] = useState<User | null>(null);
-//   const [loading, setLoading] = useState(true);
-
-//   const refreshUser = async () => {
-//     try {
-//       const response = await authAPI.me();
-
-//       // Handle different response structures
-//       if (response.data.data) {
-//         setUser(response.data.data);
-//       } else if (response.data.user) {
-//         setUser(response.data.user);
-//       } else {
-//         setUser(response.data);
-//       }
-//     } catch (error: any) {
-//       console.error("Auth check error:", error);
-//       setUser(null);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     refreshUser();
-//   }, []);
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser, loading, refreshUser }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-// export function useAuth() {
-//   const context = useContext(AuthContext);
-//   if (!context) throw new Error("useAuth must be used within AuthProvider");
-//   return context;
-// }
 
 // context/AuthContext.tsx
 "use client";
@@ -113,10 +49,6 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     } catch (error: any) {
       console.error("Auth check error:", error);
       setUser(null);
-      // اگه 401 بود یعنی نشست revoke شده
-      // if (error.response?.status === 401) {
-      //   router.push("/");
-      // }
     } finally {
       setLoading(false);
     }
