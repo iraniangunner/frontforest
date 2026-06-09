58a58d0e62c5995957ac039431d0498dffed74b9
fateh moonesi <f.moonesi@jsk.local>
2026-06-09 14:37:36 +0330

    fonts changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,39 +13,21 @@ import { UserStatusProvider } from "@/context/UserStatusContext";
 const yekanbakh = localFont({
   src: [
     {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
       path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2",
       weight: "400",
-      style: "normal",
     },
     {
       path: "../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2",
       weight: "600",
-      style: "normal",
     },
     {
       path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2",
       weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2",
-      weight: "900",
-      style: "normal",
     },
   ],
   variable: "--font-yekanbakh",
-  display: "swap",
-});
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "نمایندگی انحصاری فانتوم پلاس در ایران",
